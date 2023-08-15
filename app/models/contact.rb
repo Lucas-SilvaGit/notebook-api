48c7adaba026c573be2d25a13e436d5d289d5886
Jose Lucas <lucasnba2324@gmail.com>
2023-08-14 21:19:02 -0300

    creating methods author , kind_description and rewriting the as_json method

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,19 @@
 class Contact < ApplicationRecord
   belongs_to :kind
+
+  def author
+    "John Smith"
+  end
+  
+  def kind_description
+    self.kind.description
+  end
+
+  def as_json(options={})
+    super(
+      root: true,
+      methods: [:kind_description, :author]         #esse traz ja de forma o atributo direto na tabela
+      # include: { kind: { only: :description} }     esse traz de forma aninhada
+    )
+  end
 end
