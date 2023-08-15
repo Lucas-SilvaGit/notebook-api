d54f443ccbef8317bbb00fc31be7ff65a421c9d8
Jose Lucas <lucasnba2324@gmail.com>
2023-08-15 06:17:56 -0300

    removing phone number from contact

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_08_14_233548) do
+ActiveRecord::Schema.define(version: 2023_08_15_091607) do
 
   create_table "contacts", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.date "birthdate"
-    t.string "phone_number"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "kind_id"
