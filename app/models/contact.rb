07b11277a55494227cc009a403f5932c431ea51d
Jose Lucas <lucasnba2324@gmail.com>
2023-08-15 20:43:26 -0300

    removing unnecessary methods

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -7,20 +7,4 @@ class Contact < ApplicationRecord
   def author
     "John Smith"
   end
-  
-  def kind_description
-    self.kind.description
-  end
-
-  def phone_numbers
-    phones.pluck(:phone_number)
-  end
-
-  def as_json(options={})
-    super(
-      root: true,
-      methods: [:kind_description, :phone_numbers, :author] #esse traz ja de forma o atributo direto na tabela
-      # include: { kind: { only: :description} }     esse traz de forma aninhada
-    )
-  end
 end
