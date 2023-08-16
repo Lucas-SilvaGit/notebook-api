2e57b8a1e24bd0e688a76bc997faefd0f4dc33bf
Jose Lucas <lucasnba2324@gmail.com>
2023-08-15 21:43:59 -0300

    adding nested attributes for address

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -4,6 +4,7 @@ class Contact < ApplicationRecord
   has_one :address
 
   accepts_nested_attributes_for :phones, allow_destroy: true
+  accepts_nested_attributes_for :address
 
   def author
     "John Smith"
