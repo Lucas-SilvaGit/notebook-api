461148e7f5146afdcdd013a066eb4113c625b2b4
Jose Lucas <lucasnba2324@gmail.com>
2023-08-15 21:27:43 -0300

    adding in contact model allow_destroy for nested attributes

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,7 +2,7 @@ class Contact < ApplicationRecord
   belongs_to :kind, optional: true
   has_many :phones
 
-  accepts_nested_attributes_for :phones
+  accepts_nested_attributes_for :phones, allow_destroy: true
 
   def author
     "John Smith"
