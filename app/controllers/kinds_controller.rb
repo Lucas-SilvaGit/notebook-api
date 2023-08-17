46bcf855d88ef47d238de6d41e2e7c0c5a156ce9
Jose Lucas <lucasnba2324@gmail.com>
2023-08-17 07:02:08 -0300

    configure set kind with have params contact_id to find contact kind

--- app/controllers/kinds_controller.rb
+++ app/controllers/kinds_controller.rb
@@ -41,7 +41,13 @@ class KindsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_kind
-      @kind = Kind.find(params[:id])
+      if params[:contact_id]
+        kind_id = Contact.find(params[:contact_id]).kind_id
+      else
+        kind_id = Kind.find(params[:id])
+      end
+
+      @kind = Kind.find(kind_id)
     end
 
     # Only allow a list of trusted parameters through.
