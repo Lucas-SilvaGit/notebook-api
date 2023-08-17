9a32d0d8ed35a5222875b9a8ea1374ce6d1d8cab
Jose Lucas <lucasnba2324@gmail.com>
2023-08-17 19:57:35 -0300

    refactoring association phones to use link related

--- app/serializers/contact_serializer.rb
+++ app/serializers/contact_serializer.rb
@@ -5,7 +5,10 @@ class ContactSerializer < ActiveModel::Serializer
     link(:related) { contact_kind_url(object.id) }
   end
 
-  has_many :phones
+  has_many :phones do
+    link(:related) { contact_phones_url(object.id) }
+  end
+
   has_one :address
   
   meta do
