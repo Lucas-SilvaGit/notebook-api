14d55f4e42a087d1bf91be3a58731489849c2459
Jose Lucas <lucasnba2324@gmail.com>
2023-08-17 20:49:44 -0300

    creating methods update and address params with deserialization

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,13 +1,27 @@
 class AddressesController < ApplicationController
   before_action :set_contact
 
+  # GET /contacts/1/address
   def show
     render json: @contact.address
+  end
+
+  # PATCH /contacts/1/address
+  def update
+    if @contact.address.update(address_params)
+      render json: @contact.address
+    else
+      render json: @contact.erros, status: :unprocessable_entity
+    end
   end
 
   private
 
   def set_contact
     @contact = Contact.find(params[:contact_id])
+  end
+
+  def address_params
+    ActiveModelSerializers::Deserialization.jsonapi_parse(params)
   end
 end
