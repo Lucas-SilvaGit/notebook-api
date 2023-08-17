584fb9fd2784a6dda64638dacb28e1375a9ce3a7
Jose Lucas <lucasnba2324@gmail.com>
2023-08-16 21:25:57 -0300

    configurating links with belongs to

--- app/serializers/contact_serializer.rb
+++ app/serializers/contact_serializer.rb
@@ -1,10 +1,13 @@
 class ContactSerializer < ActiveModel::Serializer
   attributes :id, :name, :email, :birthdate
 
-  belongs_to :kind, optional: true
+  belongs_to :kind, optional: true do          
+    link(:related) { kind_url(object.kind.id) }
+  end
+
   has_many :phones
   has_one :address
-
+  
   meta do
     { author: "John Smith"}  
   end
