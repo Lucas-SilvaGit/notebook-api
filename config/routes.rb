f5cc97730307c794ab740664b9eaf8dc0ad386a6
Jose Lucas <lucasnba2324@gmail.com>
2023-08-17 07:01:11 -0300

    changing to formatter jason api routes, only show

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :kinds
-  resources :contacts
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  resources :contacts do
+    resource :kind, only: [:show]
+  end
 end
