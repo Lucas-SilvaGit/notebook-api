e728f70ad7e4094073d0fcea2a402c1fbc073f77
Jose Lucas <lucasnba2324@gmail.com>
2023-08-17 19:57:49 -0300

    creating routes for the phones controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,8 @@ Rails.application.routes.draw do
   resources :contacts do
     resource :kind, only: [:show]
     resource :kind, only: [:show], path: 'relationships/kind'
+
+    resource :phones, only: [:show]
+    resource :phones, only: [:show], path: 'relationships/phones'
   end
 end
