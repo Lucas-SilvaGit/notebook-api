d4b770ebf1a64e2e87dbad6c790935c4c5865d68
Jose Lucas <lucasnba2324@gmail.com>
2023-08-22 06:45:12 -0300

    adding include ErroSerializer

--- app/controllers/v1/contacts_controller.rb
+++ app/controllers/v1/contacts_controller.rb
@@ -1,5 +1,7 @@
 module V1
   class ContactsController < ApplicationController
+    include ErrorSerializer
+
     before_action :set_contact, only: [:show, :update, :destroy]
 
     # GET /contacts
@@ -24,7 +26,8 @@ module V1
       if @contact.save
         render json: @contact, include: [:kind, :address, :phones], status: :created, location: @contact
       else
-        render json: @contact.errors, status: :unprocessable_entity
+        render json: ErrorSerializer.serialize(@contact.errors)
+        # render json: @contact.errors, status: :unprocessable_entity
       end
     end
 
