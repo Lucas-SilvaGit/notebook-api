777006c4097669f21188b815cff867221bac6d35
Jose Lucas <lucasnba2324@gmail.com>
2023-08-24 06:43:46 -0300

    fixing applicastion controller method ensure json request

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,15 @@
 class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
-  
+
   before_action :ensure_json_request
 
   def ensure_json_request
     unless request.headers["Accept"] =~ /vnd\.api\+json/
-      render :nothing => true, :status => 406
+      render body: nil, :status => 406
     else
       unless request.get?
         return if request.headers["Content-Type"] =~ /vnd\.api\+json/
-        render :nothing => true, :status => 415
+        render body: nil, :status => 415
       end
     end
   end
