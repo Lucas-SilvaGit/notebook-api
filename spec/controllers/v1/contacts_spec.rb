c39202f60c07283677be81e9cac14e625dec60f2
Jose Lucas <lucasnba2324@gmail.com>
2023-08-24 20:27:12 -0300

    adding new test for action show and rewriting the hash method

--- spec/controllers/v1/contacts_spec.rb
+++ spec/controllers/v1/contacts_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
+class Hash
+  def json(parts)
+    ary = parts.split(">")
+    ary.reduce(self) do |memo, key|
+      memo.fetch(key.to_s.strip) if memo
+    end
+  end
+end
+
 describe V1::ContactsController, type: :controller do
   it 'request index and return 200 OK' do
     request.accept = 'application/vnd.api+json'
@@ -10,5 +19,18 @@ describe V1::ContactsController, type: :controller do
   it 'request index and return 406 NOT ACCEPTABLE' do
     get :index
     expect(response).to have_http_status(:not_acceptable)
+  end
+
+  it 'GET v1/contacts/id' do
+    contact = Contact.first
+
+    request.accept = 'application/vnd.api+json'
+    get :show, params: { id: contact.id }
+
+    response_body = JSON.parse(response.body)
+
+    expect(response_body.json('data > id')).to eq(contact.id.to_s)
+    expect(response_body.json('data > attributes > name')).to eq(contact.name)
+    expect(response_body.fetch('data').fetch('type')).to eq('contacts')
   end
 end
