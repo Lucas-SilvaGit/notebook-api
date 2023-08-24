83f124af57a2f53732c0765cc7272afe70566054
Jose Lucas <lucasnba2324@gmail.com>
2023-08-24 06:49:53 -0300

    creating tests for good and bad requests

---
+++ spec/controllers/v1/contacts_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+describe V1::ContactsController, type: :controller do
+  it 'request index and return 200 OK' do
+    request.accept = 'application/vnd.api+json'
+    get :index
+    expect(response).to have_http_status(:ok)
+  end
+
+  it 'request index and return 406 NOT ACCEPTABLE' do
+    get :index
+    expect(response).to have_http_status(:not_acceptable)
+  end
+end
